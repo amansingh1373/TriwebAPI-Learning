85b6df32e87c25616003dc5d104112514d4d68cd
Aman Singh <singh.aman200226@gmail.com>
2024-05-28 18:42:15 +0530

    implemented inheritace and used a library in java program

--- Programming/OOPs/p1.java
+++ Programming/OOPs/p1.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
+
+
+
 class Pen {
     String color;
     int price;
@@ -8,11 +12,32 @@ class Pen {
     
 }
 
-class p1 {
+class Ballpen extends Pen {
+    String tipType;
+    void write(String s) {
+        System.out.println("Writing " + s + " with Ballpen");
+    }
+}
+
+
+class Main {
     public static void main(String[] args) {
         Pen p1 = new Pen();
-        p1.color = "Blue";
-        p1.price = 10;
+        System.out.println("enter a color:");
+        Scanner sc = new Scanner(System.in);
+        p1.color = sc.nextLine();
+        System.out.println("enter the price:");
+        p1.price = sc.nextInt();
+        sc.nextLine(); 
+        Ballpen bp = new Ballpen();
+        System.out.println("enter a color for ball pen:");
+        bp.color = sc.nextLine();
+        System.out.println("enter the price for ball pen:");
+        bp.price = sc.nextInt();
+        sc.nextLine(); 
+        System.out.println("enter the tip type for ball pen:");
+        bp.tipType = sc.nextLine();
         p1.write("Hello");
+        bp.write("amazing");
     }
 }
